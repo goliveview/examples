d7a0e9fd578cbfc64ac8a47396c53678b8b09b6d
Adnaan Badr <badr.adnaan@gmail.com>
2022-02-28 07:27:22 +0100

    fix todos mod

---
+++ autocomplete/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	glv "github.com/goliveview/controller"
+)
+
+type Counter struct {
+	glv.DefaultView
+}
+
+func (c *Counter) Content() string {
+	return "app.html"
+}
+
+func (c *Counter) Partials() []string {
+	return []string{"count.html"}
+}
+
+func (c *Counter) OnMount(ctx glv.Context) (glv.Status, glv.M) {
+	return glv.Status{Code: 200}, glv.M{
+		"val": 0,
+	}
+}
+
+func (c *Counter) OnEvent(ctx glv.Context) error {
+	switch ctx.Event().ID {
+	case "inc":
+		var val int
+		ctx.Store().Get("val", &val)
+		val += 1
+		ctx.Store().Put(glv.M{"val": val})
+		ctx.DOM().Morph("#count", "count", glv.M{
+			"val": val,
+		})
+	case "dec":
+		var val int
+		ctx.Store().Get("val", &val)
+		val -= 1
+		ctx.Store().Put(glv.M{"val": val})
+		ctx.DOM().Morph("#count", "count", glv.M{
+			"val": val,
+		})
+	default:
+		log.Printf("warning:handler not found for event => \n %+v\n", ctx.Event())
+	}
+	return nil
+}
+
+func main() {
+	glvc := glv.Websocket("goliveview-counter", glv.DevelopmentMode(true))
+	http.Handle("/", glvc.Handler(&Counter{}))
+	log.Println("listening on http://localhost:9867")
+	http.ListenAndServe(":9867", nil)
+}
